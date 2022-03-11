6485381fd038006e1c6f05c325756e26f3a52327
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-03-11 19:52:44 +0530

    UC_2 : Roll The Die

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Snake And Ladder Game");
 
-int Position = 0;   
+int Position = 0;
+
+Random random = new Random();
+
+int numOnDie = random.Next(1, 7);
+Console.WriteLine("No On Die "+ numOnDie);
